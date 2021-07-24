2d8145e16ed4cf79c43b087e7ea4c004bf844cd8
AstroMyers <87773565+AstroMyers@users.noreply.github.com>
2021-07-24 01:25:52 -0600

    Add files via upload

--- RocketClockCmd.py
+++ RocketClockCmd.py
@@ -1,62 +1,77 @@
-from typing import Text
 import requests
 import json
 from time import sleep
 import os
+import platform
+from datetime import datetime
 
 url =  "https://lldev.thespacedevs.com/2.2.0/launch/upcoming/?is_crewed=false&include_suborbital=true&related=false&hide_recent_previous=true" #api to get info from
 response = requests.get(url)
 launch_info = json.loads(response.text) #get json with info
 
-launch = []
-provider = []
-mission = []
-status = []
-location = [] 
-time = []
-data = []
-
+def SystemDetect():
+    global reset
+    system = platform.system()
+    if system == 'Windows':
+        reset = 'cls'
+    elif system == 'Linux':
+        reset = 'clear'
+    os.system(reset)
+    print('System Detected: ',system)
+    sleep(1)
+    print('Fetching Data\n')
+    sleep(1)
+    os.system(reset)
+    return reset
 
 def UpdateLaunch(url): #ping API once to update to newest upcoming launch values
-
-    global data_str
-    os.system('clear')
+    global time
+    time = []
     response = requests.get(url)
     launch_info = json.loads(response.text)
-    for i in range(4):
-        provider.append(launch_info['results'][i]['launch_service_provider']['name'])
-        mission.append(launch_info['results'][i]['name'])
-        status.append(launch_info['results'][i]['status']['name'])
-        location.append(launch_info['results'][i]['pad']['name']+ ', ' + launch_info['results'][i]['pad']['location']['name'])
-        time.append(launch_info['results'][i]['net'])
+    provider = launch_info['results'][0]['launch_service_provider']['name']
+    mission =launch_info['results'][0]['name']
+    status = launch_info['results'][0]['status']['name']
+    location = launch_info['results'][0]['pad']['name']+ ', ' + launch_info['results'][0]['pad']['location']['name']
+    time = launch_info['results'][0]['net']
+    print('Provider: ', provider)
+    print('Mission: ', mission)
+    print('Status: ', status)
+    print('Location: ', location)
+    print('Time: ', time)
+    return time
     
-    for i in range(4):
-        launch = [provider[i], mission[i], status[i], location[i], time[i]]
-        
-        print('Provider: ' + launch[0])
-        print('Mission: ' + launch[1])
-        print('Status: ' + launch[2])
-        print('Location: ' + launch[3])
-        print('Time: ' + launch[4])
-        print('\n')
-        launch.append(launch)
-        sleep(10)
+def Time(time):
+    global deltatime
+    time_list = list(str(time)) #str -> list
+    time_list[10] = ' ' #remove characters
+    time_list[19] ='' #remove characters
+    time_format = ''.join(time_list) #list -> str
+    time_date = datetime.strptime(time_format,'%Y-%m-%d %H:%M:%S') #str -> date obj
+    now = datetime.now()
+    deltatime =  time_date - now 
+   
+    return deltatime
     
-        
-        
-        
-    #data_str = ' '.join(data)
-        
-    #time_list = list(time) #str -> list
-    #time_list[10] = ' ' #remove characters
-    #time_list[19] ='' #remove characters
-    #time_format = ''.join(time_list) #list -> str
-    #time_date = datetime.strptime(time_format,'%Y-%m-%d %H:%M:%S') #str -> date obj
-    #data = [provider,mission,location, status,time]
-    #data_str = ' '.join(data)
-    #width,ignore = font.getsize(data_str)
-    #return time_date, time_format, provider, mission, status, launch_info, data, data_str, width
-
-os.system('clear')
+SystemDetect()
 UpdateLaunch(url)
-#print(data_str)
+sleep(1)
+Time(time)
+while True:
+    while deltatime.total_seconds() < 1:
+        os.system(reset)
+        print('Launching!')
+        sleep(.5)
+        print('Launching!')
+        sleep(.5)
+        print('Launching!')
+        sleep(3)
+        os.system(reset)
+        print('Retreiving Next Launch...')
+        sleep(3)
+        UpdateLaunch(url)
+        break
+    while deltatime.total_seconds() > 1:
+        Time(time)
+        print(str(deltatime),end='\r')
+        break
